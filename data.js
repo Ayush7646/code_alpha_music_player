33253c7c5c57d94e968f06d862867203980ecd1c
Ayush7646 <ayushdewa1234@gmail.com>
2025-05-22 09:05:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/th1.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/th2.jpg'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/th3.jpg'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/th4.jpg'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/th5.jpg'
+    },
+]
